025d976d08e5c907f8222bf9ce56b565e48ed987
VarshneyaAmit <70054616+VarshneyaAmit@users.noreply.github.com>
2021-04-13 21:07:47 +0530

    Update Addition.py
    Please review and merge revised program

--- Addition.py
+++ Addition.py
@@ -1,6 +1,6 @@
-num1 = 1.5
-num2 = 6.3
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
 # Add two numbers
-sum = num1 + num2
-# Display the sum
+sum = float(num1) + float(num2)
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
