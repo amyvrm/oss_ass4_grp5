8dd93edc9f31912053d09f7763af14a49dd182b5
Amit Verma (TR-IN) <amit_verma@trendmicro.com>
2021-04-14 16:07:45 +0530

    Added division file

---
+++ division.py
@@ -0,0 +1,10 @@
+# division of two number
+
+print("Please enter two number for division")
+
+num1 = int(input("Enter number"))
+num2 = int(input("Enter number"))
+
+def division(num1, num2):
+    return num1/num2
+
