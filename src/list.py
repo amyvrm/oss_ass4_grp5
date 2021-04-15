d37cb791ccb37f68b4001775f477d68561ad8d74
amyvrm <amyvrm@gmail.com>
2021-04-15 22:02:41 +0530

    Made dynamic and this fixes #18

--- src/list.py
+++ src/list.py
@@ -1,11 +1,7 @@
 # a list of numbers
 numbers = [11, 22, 33, 100, 200, 300]
 
-# prints 11
-print(numbers[0])
-
-# prints 300
-print(numbers[5])
-
-# prints 22
-print(numbers[1])
+print("List numbers:")
+for ele in numbers:
+  print(ele)
+  
